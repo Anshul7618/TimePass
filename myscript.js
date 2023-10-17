c838ee62260e0ef3a4fb32513497fa237c19e35a
Anshul Bansal <120884064+Anshul7618@users.noreply.github.com>
2023-10-17 10:15:38 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -14,4 +14,6 @@ noBtn.addEventListener('click', () => {
 const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
 const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
 
+  noBtn.style.left = i + 'px';
+  noBtn.style.top = j + 'px';
 });
