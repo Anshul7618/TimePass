392a2148457fdf345992491e6fd3efea308dbd9d
Anshul Bansal <120884064+Anshul7618@users.noreply.github.com>
2023-10-17 10:06:02 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -14,6 +14,4 @@ noBtn.addEventListener('mouseover', () => {
 const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
 const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
 
-noBtn.style.left = i + 'px';
-noBtn.style.top = j + 'px';
 });
